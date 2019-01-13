136081aceb1c074ec31a9dc344ecb903fe4632b0
flunk72 <40355944+flunk72@users.noreply.github.com>
2019-01-13 18:06:37 +0500

    +2 урок

---
+++ src/components/Link.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const StyledLink = styled.a
+`
+  font-size: 14px;
+  color: ${props => props.active ? "pink" : "#fff"};
+  line-height: 24px;
+  font-weight: 300px;
+  padding-right: 33px;
+  border-right: 1px dotted #fff;
+
+  &:hover {
+      color: #8d8d8d;
+      text-decaration: none;
+  }
+  &: last-child {
+      border-right: none;
+  }
+`
+class Link extends React.Component {
+    render() {
+        return(
+          <StyledLink href={this.props.link}> {this.props.text} </StyledLink>  
+        )
+    }
+}
+
+export default Link
